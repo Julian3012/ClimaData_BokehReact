cb4dea4f2952de4935ec4ba8fbe3f4a60c864759
noppelmax <gitkraken@noppelmax.online>
2019-01-29 22:32:21 +0100

    small dialog

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
-from os.path import dirname, join
+import os
 
 import numpy as np
-import pandas.io.sql as psql
-import sqlite3 as sql
 
 from bokeh.plotting import figure
 from bokeh.layouts import layout, widgetbox, row
 from bokeh.models import ColumnDataSource, Div, PreText
 from bokeh.models.widgets import Slider, Select, TextInput
-from bokeh.io import curdoc
+from bokeh.io import curdoc, show
 
 import bokeh as bokeh
 import pandas as pd
@@ -18,39 +16,33 @@ import holoviews as hv
 import numpy as np
 import dask
 import datashader as ds
+
 from datashader.bokeh_ext import InteractiveImage
-from holoviews.operation.datashader import datashade, shade, dynspread, rasterize
-from holoviews.operation import decimate
-import datashader.utils as du, datashader.transfer_functions as tf
+from holoviews.operation.datashader import datashade
 
 import geoviews as gv
 from cartopy import crs
 import geoviews.feature as gf
 from scipy.spatial import Delaunay
 
-hv.extension('bokeh')
 
-options = hv.Store.options(backend='bokeh')
-options.Points = hv.Options('plot', width=800, height=600, size_index=None,)
-xrData = xr.open_dataset("/home/max/Downloads/2016033000-ART-passive_grid_pmn_DOM01_ML_0002.nc",decode_cf=False)
-verts = np.column_stack((xrData.clon_bnds.stack(z=('vertices','ncells')),xrData.clat_bnds.stack(z=('vertices','ncells'))))
+renderer = hv.renderer('bokeh').instance(mode='server')
 
-def fnt(verts):
+START = 0
+LOADING = 1
+LOADED = 2
 
-    #pts = np.column_stack((xrData.clon,xrData.clat,xrData.isel(height=0,time=0).TR_stn))
-    #print(pts)
+state = START
 
-    #verts = np.append(verts,[0,0])
-    #print(verts.shape)
+def graph(url):
     n1 = []
     n2 = []
     n3 = []
 
+    xrData = xr.open_dataset("/home/max/Downloads/2016033000-ART-passive_grid_pmn_DOM01_ML_0002.nc",decode_cf=False)
+    verts = np.column_stack((xrData.clon_bnds.stack(z=('vertices','ncells')),xrData.clat_bnds.stack(z=('vertices','ncells'))))
+
     l = len(xrData.clon_bnds)
-    #for i in range(0,l):
-    #    n1.append([i])
-    #    n2.append([i+l])
-    #    n3.append([i+l+l])
 
     n1 = np.arange(l)
     n2 = n1 + l
@@ -58,7 +50,6 @@ def fnt(verts):
 
     n4 = np.column_stack((n1,n2,n3))
     n = np.column_stack((n4,xrData.isel(height=0,time=0).TR_stn))
-    #n = np.column_stack((n1,n2,n3))
 
     verts = pd.DataFrame(verts,  columns=['x', 'y'])
     tris  = pd.DataFrame(n, columns=['v0', 'v1', 'v2','TR_stn'], dtype = np.float64)
@@ -68,14 +59,49 @@ def fnt(verts):
 
     print('vertices:', len(verts), 'triangles:', len(tris))
 
-    #mesh = du.mesh(verts,tris)
-    #cvs = ds.Canvas(plot_height=900, plot_width=900)
-    #agg = cvs.trimesh(verts, tris, mesh=mesh)
-    #tf.Image(tf.shade(agg))
-
-    #datashade(hv.TriMesh((tris,verts), vdims=["TR_stn"], label="Wireframe").options(filled=True))
     return datashade(hv.TriMesh((tris,verts), label="Wireframe").options(filled=True))
 
-shaded = fnt(verts)
-doc = hv.renderer('bokeh').server_doc(shaded)
-doc.title = 'HoloViews Bokeh App'
+def loadCallback():
+    state = LOADING
+
+    divLoading = Div(text="loading...")
+    curdoc().clear()
+    l = layout([
+    [widgetbox(divLoading)]
+    ])
+    curdoc().add_root(l)
+
+    plot = renderer.get_plot(graph(""))
+
+    curdoc().clear()
+    l = layout([
+    [plot.state]
+    ])
+    curdoc().add_root(l)
+    state = LOADED
+
+def modify_doc(doc):
+    doc.title = 'HoloViews Bokeh App'
+    urlinput = TextInput(value="default", title="netCFD/OpenDAP Source URL:")
+    btLoad = bokeh.models.Button(label="load")
+    btLoad.on_click(loadCallback)
+
+    doc.clear()
+    l = layout([
+    [widgetbox(urlinput)],
+    [widgetbox(btLoad)]
+    ])
+    doc.add_root(l)
+
+
+
+#urlinput = TextInput(value="default", title="netCFD/OpenDAP Source URL:")
+#btLoad = bokeh.models.Button(label="load")
+#btLoad.on_click(loadCallback)
+
+
+#shaded = graph("")
+#doc = hv.renderer('bokeh').server_doc(shaded)
+#doc.title = 'HoloViews Bokeh App'
+
+modify_doc(curdoc())
