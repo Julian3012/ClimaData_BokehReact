cc84c90e4e85959bffd34601f2990da8b6a7c9bd
noppelmax <gitkraken@noppelmax.online>
2019-03-12 15:01:57 +0100

    Implement object orientend, fix non-free-dimension because of length == 0

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ from cartopy import crs
 
 from holoviews.operation.datashader import datashade, rasterize
 
-
-
 import math
 
 renderer = hv.renderer('bokeh').instance(mode='server',size=300)
@@ -36,17 +34,14 @@ urlinput = TextInput(value="default", title="netCFD/OpenDAP Source URL:")
 slVar = None
 slMesh = None
 slCMap = None
-variable = ""
+txTitle = None
 
-
-n = None
-tris = None
-verts = None
-
-freedims = []
 aggregates = []
+
 COLORMAPS = ["Inferno","Magma","Plasma","Viridis","BrBG","PiYG","PRGn","PuOr","RdBu","RdGy","RdYlBu","RdYlGn","Spectral","Blues","BuGn","BuPu","GnBu","Greens","Greys","Oranges","OrRd","PuBu","PuBuGn","PuRd","Purples","RdPu","Reds","YlGn","YlGnBu","YlOrBr","YlOrRd"]
 
+tmPlot = None
+xrData = None
 
 class Aggregates():
     def __init__(self, dim, f):
@@ -54,12 +49,93 @@ class Aggregates():
         self.f = f
 
 class TriMeshPlot():
-    def __init__(self, xrData, cm, tris, verts):
+    def __init__(self, xrData, tris, verts, cm="Magma"):
         self.xrData = xrData
         self.cm = cm
         self.tris = tris
         self.verts = verts
 
+    def getPlotObject(self, variable, title, cm="None"):
+        self.variable = variable
+
+        if cm is not "None":
+            self.cm = cm
+        print(self.cm)
+        self.title = title
+        return self.buildDynamicMaps()
+
+    def buildDynamicMaps(self):
+        """
+        Function to capsulate the free dimensions together with the actual
+        Trimeshgraph.
+
+        Returns:
+            : a rasterizes plot of the DynamicMap with the TriMesh graph in it.
+        """
+        self.freeDims = []
+        self.nonFreeDims = []
+        for d in getattr(self.xrData,self.variable).dims:
+            # WORKAROUND because Holoview is not working with a kdim with name "height"
+            # See issue https://github.com/pyviz/holoviews/issues/3448
+            if d == "height":
+                self.freeDims.append("hi")
+                continue
+            if d != "ncells" and (len(getattr(getattr(self.xrData,self.variable),d))-1) > 0:
+                self.freeDims.append(d)
+            if d != "ncells" and (len(getattr(getattr(self.xrData,self.variable),d))-1) == 0:
+                self.nonFreeDims.append(d)
+
+        ranges = {}
+        for d in self.freeDims:
+            # WORKAROUND because Holoview is not working with a kdim with name "height"
+            # See issue https://github.com/pyviz/holoviews/issues/3448
+            if d != "hi":
+                ranges[d] = (0,len(getattr(getattr(self.xrData,self.variable),d))-1)
+            else:
+                ranges[d] = (0,len(getattr(getattr(self.xrData,self.variable),"height"))-1)
+        dm = hv.DynamicMap(self.buildTrimeshbuildDynamicMap, kdims=self.freeDims).redim.range(**ranges)
+
+        return rasterize(dm).opts(cmap=self.cm,colorbar=True)
+
+    def buildTrimeshbuildDynamicMap(self, *args):
+        """
+        Function that builds up the TriMesh-Graph
+        Args:
+            Take multiple arguments. A value for every free dimension.
+        Returns:
+            The TriMesh-Graph object
+        """
+
+        # Define aggregates
+        #aggregateFunctions = ["mean","sum"]
+        #aggregateDimensions = ["lon","height"]
+        #slAggregateFunction = bokeh.models.Select(title="Aggregate Function", options=aggregateFunctions, value="mean")
+        #slAggregateDimension = bokeh.models.Select(title="Aggregate Dimension", options=aggregateDimensions, value="lon")
+
+        selectors = {}
+        idx = 0
+        # Build the data selector for the free dimensions. For those are the DynamicMap shows sliders to the right of the graph
+        for d in self.freeDims:
+            # WORKAROUND because Holoview is not working with a kdim with name "height"
+            # See issue https://github.com/pyviz/holoviews/issues/3448
+            if d == "hi":
+                selectors["height"] = args[idx]
+            else:
+                selectors[d] = args[idx]
+            idx = idx +1
+
+        # Also select non-free-dimensions. Those are dimensions that are of length 1
+        for d in self.nonFreeDims:
+            if d == "hi":
+                selectors["height"] = 0
+            else:
+                selectors[d] = 0
+            idx = idx +1
+
+        self.tris["var"] = getattr(self.xrData, self.variable).isel(selectors)
+
+        res = hv.TriMesh((self.tris,self.verts), label=(self.title))
+        return res
 
 def getURL():
     """
@@ -72,42 +148,23 @@ def getURL():
     url = "/home/max/Downloads/2016033000-ART-passive_grid_pmn_DOM01_ML_0002.nc"
     return url
 
-def buildDynamicMap():
+def loadData(url):
     """
-    Function to capsulate the free dimensions together with the actual
-    Trimeshgraph.
+    Function load OPeNDAP data
 
     Returns:
-        : a rasterizes plot of the DynamicMap with the TriMesh graph in it.
+        xarray Dataset: Loads the url as xarray Dataset
     """
-    for d in getattr(xrData,variable).dims:
-        # WORKAROUND because Holoview is not working with a kdim with name "height"
-        # See issue https://github.com/pyviz/holoviews/issues/3448
-        if d == "height":
-            freedims.append("hi")
-            continue
-        if d != "ncells":
-            freedims.append(d)
+    xrData = xr.open_dataset(url,decode_cf=False)
+    return xrData
 
-    ranges = {}
-    for d in freedims:
-        # WORKAROUND because Holoview is not working with a kdim with name "height"
-        # See issue https://github.com/pyviz/holoviews/issues/3448
-        if d != "hi":
-            ranges[d] = (0,len(getattr(getattr(xrData,variable),d)))
-        else:
-            ranges[d] = (0,len(getattr(getattr(xrData,variable),"height")))
-    dm = hv.DynamicMap(buildTrimeshbuildDynamicMap, kdims=freedims).redim.range(**ranges)
+def loadMesh(xrData):
+    """
+    Function to build up a mesh
 
-
-    cm = "Magma"
-    if slCMap is not None:
-        cm = slCMap.value
-
-    return rasterize(dm).opts(cmap=cm,colorbar=True)
-
-def loadDataAndMesh():
-    xrData = xr.open_dataset(getURL(),decode_cf=False)
+    Returns:
+        array of triangles and vertices: Builds the mesh from the loaded xrData
+    """
     verts = np.column_stack((xrData.clon_bnds.stack(z=('vertices','ncells')),xrData.clat_bnds.stack(z=('vertices','ncells'))))
 
     #not so performant
@@ -125,56 +182,14 @@ def loadDataAndMesh():
     n2 = n1 + l
     n3 = n2 + l
 
-    n4 = np.column_stack((n1,n2,n3))
-    n = np.column_stack((n4,np.arange(l)))
+    n = np.column_stack((n1,n2,n3,n1))
 
     verts = pd.DataFrame(verts,  columns=['Longitude', 'Latitude'])
     tris  = pd.DataFrame(n, columns=['v0', 'v1', 'v2',"var"], dtype = np.float64)
     tris['v0'] = tris["v0"].astype(np.int32)
     tris['v1'] = tris["v1"].astype(np.int32)
     tris['v2'] = tris["v2"].astype(np.int32)
-    return (tris,verts, xrData)
-
-def buildTrimeshbuildDynamicMap(*args):
-    """
-    Function that builds up the TriMesh-Graph
-    Args:
-        Take multiple arguments. A value for every free dimension.
-    Returns:
-        The TriMesh-Graph object
-    """
-    global tris, verts, xrData, variable, freedims
-
-
-    # Define aggregates
-    aggregateFunctions = ["mean","sum"]
-    aggregateDimensions = ["lon","height"]
-    slAggregateFunction = bokeh.models.Select(title="Aggregate Function", options=aggregateFunctions, value="mean")
-    slAggregateDimension = bokeh.models.Select(title="Aggregate Dimension", options=aggregateDimensions, value="lon")
-
-    selectors = {}
-    idx = 0
-    for d in freedims:
-        # WORKAROUND because Holoview is not working with a kdim with name "height"
-        # See issue https://github.com/pyviz/holoviews/issues/3448
-        if d == "hi":
-            selectors["height"] = args[idx]
-        else:
-            selectors[d] = args[idx]
-        idx = idx +1
-
-    if tris is None:
-        (trisN, vertsN, xrDataN) = loadDataAndMesh()
-        tris  = trisN
-        verts = vertsN
-        xrData = xrDataN
-
-    tris["var"] = getattr(xrData, variable).isel(selectors)
-
-    print('vertices:', len(verts), 'triangles:', len(tris))
-
-    res = hv.TriMesh((tris,verts), label=(variable))
-    return res
+    return (tris,verts)
 
 def prebuildDynamicMapingDialog():
     global slVar, slMesh, xrData
@@ -240,8 +255,8 @@ def cmapUpdate(attr, old, new):
     mainbuildDynamicMapDialog()
 
 def mainbuildDynamicMapDialog():
-    global xrData, height, variable
-    global slVar, slCMap
+    global slVar, slCMap, txTitle
+    global tmPlot, xrData
 
     state =LOADING
 
@@ -256,22 +271,29 @@ def mainbuildDynamicMapDialog():
     for k,v in xrData.variables.items():
         variables.append(k)
 
-    slVar = bokeh.models.Select(title="Variable", options=variables, value=variable)
+    if slVar is None:
+        slVar = bokeh.models.Select(title="Variable", options=variables, value="None")
 
-    slCMap = bokeh.models.Select(title="Colormap", options=COLORMAPS, value=cm)
-    txTitle = bokeh.models.TextInput(value="TR_stn, height ...", title="Title:")
+    if slCMap is None:
+        slCMap = bokeh.models.Select(title="Colormap", options=COLORMAPS, value="Magma")
+
+    if txTitle is None:
+        txTitle = bokeh.models.TextInput(value="TR_stn, height ...", title="Title:")
+
     txPre = bokeh.models.PreText(text=str(xrData),width=800)
     cbOpts = bokeh.models.CheckboxButtonGroup(
-        labels=["Colorbar", "x-Axis", "y-Axis"], active=[0, 1, 1])
-
+        labels=["Colorbar", "x-Axis", "y-Axis"], active=[1, 1, 1])
 
     btShow = bokeh.models.Button(label="show")
     btShow.on_click(mainbuildDynamicMapDialog)
+
     slVar.on_change("value",variableUpdate)
     slCMap.on_change("value",cmapUpdate)
 
-    variable = slVar.value
 
+    variable = slVar.value
+    title = txTitle.value
+    cm = slCMap.value
 
     divLoading = Div(text="loading buildDynamicMap...")
     curdoc().clear()
@@ -280,7 +302,14 @@ def mainbuildDynamicMapDialog():
     ])
     curdoc().add_root(l)
 
-    plot = renderer.get_widget(buildDynamicMap(),'widgets')
+    if xrData is None:
+        xrData = loadData(getURL())
+
+    if tmPlot is None:
+        (tris, verts) = loadMesh(xrData)
+        tmPlot = TriMeshPlot(xrData, tris, verts, cm=cm)
+
+    plot = renderer.get_widget(tmPlot.getPlotObject(variable=variable,title=title,cm=cm),'widgets')
     print(plot.state)
     curdoc().clear()
     l = layout([
